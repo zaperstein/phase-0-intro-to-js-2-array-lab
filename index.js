f6db48c9d49259a321df3ce96ef3f018f1dd311e
Maggie Saperstein <maggie.saperstein@gmail.com>
2022-05-19 11:25:31 -0500

    commit tree 2af17e36dff45f30fe5ec0f3bbcfca0eefe419f4

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  return cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+  return cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(name) {
+  return cats.pop();
+}
+
+function destructivelyRemoveFirstCat(name) {
+  return cats.shift();
+}
+
+function appendCat(name) {
+  const newCats = [...cats, name]
+  return newCats;
+}
+
+function prependCat(name){
+  const newCats = [name, ...cats]
+  return newCats;
+}
+
+function removeLastCat() {
+  const noLastCat = cats.slice(0, -1);
+  return noLastCat;
+}
+
+function removeFirstCat(){
+  const noFirstCat = cats.slice(1);
+  return noFirstCat
+}q
